f8f3f9814638e77a44a746b53b187fc10cabdff9
JLKwong <JLeeKwong@gmail.com>
2021-05-20 18:17:34 -0700

    temporary Reminder message fix

--- modules/remind.sh
+++ modules/remind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function remind () {
-  Reminder="\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send \\\`!d bump\\\` to support this server. :pleading_face: Each successful bump averages about 1 new member.\""
+  Reminder="\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\""
   LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "" | sed 's/^"\(.*\)"$/\1/')
   echo $Reminder
   message discord send $Reminder
