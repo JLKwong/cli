2bfbfbc9a6a0a713542c3902dbddf2cf04c0157a
JLKwong <JLeeKwong@gmail.com>
2021-05-20 17:15:18 -0700

    fix syntax remind.sh

--- modules/remind.sh
+++ modules/remind.sh
@@ -2,7 +2,7 @@
 
 function remind () {
   LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "")
-if [ $LastMessage = "!bump" ]; then
-  message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send `!d bump` to support this server. :pleading_face: Each successful bump averages about 1 new member."
+  if [ $LastMessage = "!bump" ]; then
+    message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send `!d bump` to support this server. :pleading_face: Each successful bump averages about 1 new member."
+  fi
 }
-fi
