fa734e132905d5f684acf6899d56de87c87c2936
JLKwong <JLeeKwong@gmail.com>
2021-05-20 17:01:32 -0700

    test remind.sh

--- modules/remind.sh
+++ modules/remind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 function remind () {
-message discord send "The Global Language Federation" "bot-spam" '!d bump'
+  message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send \`!d bump\` to support this server. :pleading_face: Each successful bump averages about 1 new member."
 }
