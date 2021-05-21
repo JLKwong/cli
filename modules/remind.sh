f6abf8bf2ea0706e70accfb6792c6c35cb6d5c7e
JLKwong <JLeeKwong@gmail.com>
2021-05-20 17:34:12 -0700

    debug Discord message remind.sh

--- modules/remind.sh
+++ modules/remind.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function remind () {
-  Reminder=$(message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send \`!d bump\` to support this server. :pleading_face: Each successful bump averages about 1 new member.")
+  Reminder="Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send \`!d bump\` to support this server. :pleading_face: Each successful bump averages about 1 new member."
   LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "")
   echo "  BEFORE CONDITIONAL"
   if [ "$LastMessage" = '!bump' ]; then
-    echo $Reminder
+    message discord send $Reminder
   echo "  INSIDE CONDITIONAL"
   fi
 }
