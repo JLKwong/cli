310c4feb093f3f40601d8cc8a6ab414039dab105
JLKwong <JLeeKwong@gmail.com>
2021-05-20 18:28:42 -0700

    debug reminder message

--- modules/remind.sh
+++ modules/remind.sh
@@ -3,9 +3,13 @@
 function remind () {
   Reminder=$(echo "\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\"")
   LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "" | sed 's/^"\(.*\)"$/\1/')
-  echo $Reminder
-  message discord send $Reminder
-  if [ "$LastMessage" == "!bump" ]; then
-    message discord send "$Reminder"
-  fi
+  echo "  Reminder: $Reminder"
+  echo
+  echo
+  echo
+  echo "  Last Message: $LastMessage"
+  #message discord send $Reminder
+  #if [ "$LastMessage" == "!bump" ]; then
+  #  message discord send "$Reminder"
+  #fi
 }
