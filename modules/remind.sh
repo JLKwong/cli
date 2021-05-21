ec564966f5719f13db5102ed998bd5538a8f82a6
JLKwong <JLeeKwong@gmail.com>
2021-05-20 22:51:14 -0700

    troubleshoot conditional

--- modules/remind.sh
+++ modules/remind.sh
@@ -6,7 +6,7 @@ function remind () {
   #echo "  Reminder: $Reminder"
   #echo
   #echo
-  #echo "  Last Message: $LastMessage"
+  echo "  Last Message: $LastMessage"
   #message discord send $Reminder
   #LastMessage="!bump"
   #Reminder="\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\""
