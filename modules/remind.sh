f9d83ce9ed33ff234c593462e89c17b4cbaa4edc
JLKwong <JLeeKwong@gmail.com>
2021-05-20 22:47:58 -0700

    brute force reminder message

--- modules/remind.sh
+++ modules/remind.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 function remind () {
-  Reminder=$(echo "\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\"")
+  #Reminder=$(echo "\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\"")
   LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "" | sed 's/^"\(.*\)"$/\1/')
-  echo "  Reminder: $Reminder"
-  echo
-  echo
+  #echo "  Reminder: $Reminder"
+  #echo
+  #echo
   #echo "  Last Message: $LastMessage"
   #message discord send $Reminder
   LastMessage="!bump"
-  Reminder="\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\""
-  echo "  Reminder:  $Reminder"
-  #if [ "$LastMessage" == "!bump" ]; then
-  #  message discord send $Reminder
-  #fi
+  #Reminder="\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\""
+  #echo "  Reminder:  $Reminder"
+  if [ "$LastMessage" == "!bump" ]; then
+    message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" '@here It has been 2 hours! Please send `!d bump` to support this server. :pleading_face: Each successful bump averages about 1 new member.'
+  fi
 }
