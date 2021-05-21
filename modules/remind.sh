6242971377a8abfa70c8fba7698a0497092ca07c
JLKwong <JLeeKwong@gmail.com>
2021-05-20 17:31:45 -0700

    debug Discord message remind.sh

--- modules/remind.sh
+++ modules/remind.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function remind () {
-  message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send \`!d bump\` to support this server. :pleading_face: Each successful bump averages about 1 new member."
-  #LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "")
-  #echo "Last Message: $LastMessage"
-  #if [ "$LastMessage" = '!bump' ]; then
-  #  message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send `!d bump` to support this server. :pleading_face: Each successful bump averages about 1 new member."
-  #echo "Last Message: $LastMessage"
-  #fi
+  Reminder=$(message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" "@here It's been 2 hours! Please send \`!d bump\` to support this server. :pleading_face: Each successful bump averages about 1 new member.")
+  LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "")
+  echo "Last Message: $LastMessage"
+  if [ "$LastMessage" = '!bump' ]; then
+    echo $Reminder
+  echo "Last Message: $LastMessage"
+  fi
 }
