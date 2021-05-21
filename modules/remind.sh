5270631c07445bdca9aa9bc26bb75c18286141f5
JLKwong <JLeeKwong@gmail.com>
2021-05-20 23:03:05 -0700

    test while loop

--- modules/remind.sh
+++ modules/remind.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 function remind () {
+  while true; do
   #Reminder=$(echo "\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\"")
-  LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "" | sed 's/^"\(.*\)"$/\1/')
+    LastMessage=$(message discord read "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" | grep -m1 "" | sed 's/^"\(.*\)"$/\1/')
   #echo "  Reminder: $Reminder"
   #echo
   #echo
@@ -11,9 +12,11 @@ function remind () {
   #LastMessage="!bump"
   #Reminder="\"Deaf & Hard of Hearing Community (and Allies)\" \"staff-bot\" \"@here It's been 2 hours! Please send bump to support this server. :pleading_face: Each successful bump averages about 1 new member.\""
   #echo "  Reminder:  $Reminder"
-  if [ "$LastMessage" == "!bump" ]; then
-    message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" '@here It has been 2 hours! Please send `!d bump` to support this server. :pleading_face: Each successful bump averages about 1 new member.'
-  else
-    message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" '@here asdfiovjiaosemre :pensive: Please send `!d bump`. :pleading_face: Over 6 hours have passed already. :pensive:'
-  fi
+    if [ "$LastMessage" == "!bump" ]; then
+      message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" '@here It has been 2 hours! Please send `!d bump` to support this server. :pleading_face: Each successful bump averages about 1 new member.'
+    else
+      message discord send "Deaf & Hard of Hearing Community (and Allies)" "staff-bot" '@here asdfiovjiaosemre :pensive: Please send `!d bump`. :pleading_face: Over 6 hours have passed already. :pensive:'
+    fi
+    sleep 5
+  done
 }
